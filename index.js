d59d30cb0365bc3b81fade5e10bfeb613c397cbb
FraCav99 <MasterCaster36@outlook.it>
2020-08-22 16:51:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const burger = document.querySelector('.burger-menu');
+const nav = document.querySelector('.nav-links');
+const menuLinks = document.querySelectorAll('.fading-links li');
+
+
+// Toggle Nav
+burger.addEventListener('click', () => {
+
+    // Burger Animation
+    burger.classList.toggle('toggle');
+
+    // show the menu
+    nav.classList.toggle('nav-active');
+});
+
+
+navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    })
+})
